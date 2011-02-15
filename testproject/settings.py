3e64c722188eead32ef16936ca315777949fd373
Jonas Obrist <jonas.obrist@divio.ch>
2011-02-15 19:20:13 +0100

    added xml reports for test runs

--- testproject/settings.py
+++ testproject/settings.py
@@ -1,7 +1,20 @@
 # -*- coding: utf-8 -*-
+import django
 import os
+import sys
 
 PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
+
+PYTHON_VERSION = '%s.%s' % sys.version_info[:2]
+DJANGO_VERSION = django.get_version()
+
+JUNIT_OUTPUT_DIR = os.path.join(
+    PROJECT_PATH,
+    '..',
+    'junit-dj%s-py%s' % (DJANGO_VERSION, PYTHON_VERSION)
+)
+
+TEST_RUNNER = 'testproject.testrunner.TestSuiteRunner'
 
 DATABASE_ENGINE = 'sqlite3'
 DATABASE_NAME = 'nani.sqlite'
