d3e742ff86a0cf68968e07072953e50afbb6b784
Kristian Øllegaard <kristian.ollegaard@divio.ch>
2011-04-07 15:52:18 +0200

    Splittet unittest into smaller defs and added data validation on the modelform

--- nani/tests/forms.py
+++ nani/tests/forms.py
@@ -41,6 +41,8 @@ class FormTests(NaniTestCase):
         form = NormalForm()
         self.assertTrue("translated_field" in form.fields)
         self.assertTrue("shared_field" in form.fields)
+        self.assertTrue("translated_field" in form.base_fields)
+        self.assertTrue("shared_field" in form.base_fields)
         self.assertFalse(form.is_valid())
         
         # Check if it works with media argument too
@@ -61,18 +63,34 @@ class FormTests(NaniTestCase):
             'translated_field': TRANSLATED,
             'language_code': 'en'
         }
-        
-        # Check if it accepts data and outputs the right fields
         form = NormalForm(data)
         self.assertTrue(form.is_valid(), form.errors.as_text())
         self.assertTrue("translated_field" in form.fields)
         self.assertTrue("shared_field" in form.fields)
+        self.assertTrue(TRANSLATED in form.clean()["translated_field"])
+        self.assertTrue(SHARED in form.clean()["shared_field"])
         
+    def test_normal_model_form_initaldata_instance(self):
         # Check if it accepts inital data and instance
+        SHARED = 'Shared'
+        TRANSLATED = 'English'
+        data = {
+            'shared_field': SHARED,
+            'translated_field': TRANSLATED,
+            'language_code': 'en'
+        }
         form = NormalForm(data, instance=Normal(), initial=data)
         self.assertTrue(form.is_valid(), form.errors.as_text())
         
+    def test_normal_model_form_existing_instance(self):
         # Check if it works with an existing instance of Normal
+        SHARED = 'Shared'
+        TRANSLATED = 'English'
+        data = {
+            'shared_field': SHARED,
+            'translated_field': TRANSLATED,
+            'language_code': 'en'
+        }
         instance = Normal.objects.language("en").create(shared_field=SHARED, translated_field=TRANSLATED)
         form = NormalForm(instance=instance)
         self.assertFalse(form.is_valid())
