97e72676ff27e4d724adc24a0ede8defd517e2b6
Jonas Obrist <jonas.obrist@divio.ch>
2011-05-13 17:48:44 +0200

    Improved order_by tests Added reverse test

--- nani/tests/ordering.py
+++ nani/tests/ordering.py
@@ -5,13 +5,42 @@ from testproject.app.models import Normal
 
 class OrderingTest(NaniTestCase):
     def test_minus_order_by(self):
-        with LanguageOverride("en"):
-            obj = Normal(language_code='en', translated_field = "English", shared_field="lol")
-            obj.save()
-            self.assertEqual(Normal.objects.order_by('-shared_field').count(), 1)
+        a = Normal.objects.language('en').create(translated_field = "English",
+                                                 shared_field="A")
+        b = Normal.objects.language('en').create(translated_field = "English",
+                                                 shared_field="B")
+        qs = Normal.objects.language('en').order_by('-shared_field')
+        self.assertEqual(qs.count(), 2)
+        self.assertEqual(qs[0].pk, b.pk)
+        self.assertEqual(qs[1].pk, a.pk)
+        
+    def test_order_by(self):
+        a = Normal.objects.language('en').create(translated_field = "English",
+                                                 shared_field="A")
+        b = Normal.objects.language('en').create(translated_field = "English",
+                                                 shared_field="B")
+        qs = Normal.objects.language('en').order_by('shared_field')
+        self.assertEqual(qs.count(), 2)
+        self.assertEqual(qs[0].pk, a.pk)
+        self.assertEqual(qs[1].pk, b.pk)
     
     def test_random_order(self):
-        with LanguageOverride("en"):
-            obj = Normal(language_code='en', translated_field = "English", shared_field="lol")
-            obj.save()
-            self.assertEqual(Normal.objects.order_by('?').count(), 1)
+        a = Normal.objects.language('en').create(translated_field = "English",
+                                                 shared_field="A")
+        b = Normal.objects.language('en').create(translated_field = "English",
+                                                 shared_field="B")
+        qs = Normal.objects.language('en').order_by('?')
+        self.assertEqual(qs.count(), 2)
+        pks = [obj.pk for obj in qs]
+        self.assertTrue(a.pk in pks)
+        self.assertTrue(b.pk in pks)
+        
+    def test_reverse(self):
+        a = Normal.objects.language('en').create(translated_field = "English",
+                                                 shared_field="A")
+        b = Normal.objects.language('en').create(translated_field = "English",
+                                                 shared_field="B")
+        qs = Normal.objects.language('en').order_by('shared_field').reverse()
+        self.assertEqual(qs.count(), 2)
+        self.assertEqual(qs[0].pk, b.pk)
+        self.assertEqual(qs[1].pk, a.pk)
