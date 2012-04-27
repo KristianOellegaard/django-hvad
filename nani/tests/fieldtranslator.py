afe93b6c74ef6879368658e9405c008d19ff9fd9
ping-win <pingwin64@gmail.com>
2012-04-27 15:47:11 +0400

    Fixed tests. Now the tests support the work of records with null relations

--- nani/tests/fieldtranslator.py
+++ nani/tests/fieldtranslator.py
@@ -9,14 +9,14 @@ class FieldtranslatorTests(NaniTestCase):
         INPUT = 'normal__shared_field'
         query_string, joins = translate(INPUT, Related)
         self.assertEqual(query_string, INPUT)
-        self.assertEqual(joins, ['normal__translations__language_code'])
-        
+        self.assertEqual(joins, [('normal__translations__language_code', 'normal')])
+
     def test_query_bit(self):
         INPUT = 'normal__shared_field__exact'
         query_string, joins = translate(INPUT, Related)
         self.assertEqual(query_string, INPUT)
-        self.assertEqual(joins, ['normal__translations__language_code'])
+        self.assertEqual(joins, [('normal__translations__language_code', 'normal')])
         INPUT = 'normal__translated_field__exact'
         query_string, joins = translate(INPUT, Related)
         self.assertEqual(query_string, 'normal__translations__translated_field__exact')
-        self.assertEqual(joins, ['normal__translations__language_code'])
+        self.assertEqual(joins, [('normal__translations__language_code', 'normal')])
