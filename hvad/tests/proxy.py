6ca87e284cb45ffcc051cf4001775b1d7f62710b
Iván Raskovsky <raskovsky+git@gmail.com>
2012-09-10 21:05:26 -0300

    Fixed returned models from querysets and forms for proxy models
    The combined shared and translation models now get casted to the correct
    model.

--- hvad/tests/proxy.py
+++ hvad/tests/proxy.py
@@ -29,3 +29,19 @@ class ProxyTests(NaniTestCase):
         # creation
         NormalProxyProxy.objects.language('en').create(shared_field='SHARED', translated_field='English')
         self.assertEqual(NormalProxyProxy.objects.language('en').count(), 1)
+
+    def test_translation_queryset(self):
+        NormalProxy.objects.language('en').create(shared_field='SHARED2', translated_field='English2')
+        self.assertTrue(isinstance(Normal.objects.language('en').get(), Normal))
+        self.assertFalse(isinstance(Normal.objects.language('en').get(), NormalProxy))
+        self.assertTrue(isinstance(NormalProxy.objects.language('en').get(), NormalProxy))
+        self.assertFalse(isinstance(NormalProxy.objects.language('en').get(), NormalProxyProxy))
+        self.assertTrue(isinstance(NormalProxyProxy.objects.language('en').get(), NormalProxyProxy))
+
+    def test_fallback_queryset(self):
+        NormalProxyProxy.objects.language('en').create(shared_field='SHARED2', translated_field='English2')
+        self.assertTrue(isinstance(Normal.objects.untranslated().use_fallbacks().get(), Normal))
+        self.assertFalse(isinstance(Normal.objects.untranslated().use_fallbacks().get(), NormalProxy))
+        self.assertTrue(isinstance(NormalProxy.objects.untranslated().use_fallbacks().get(), NormalProxy))
+        self.assertFalse(isinstance(NormalProxy.objects.untranslated().use_fallbacks().get(), NormalProxyProxy))
+        self.assertTrue(isinstance(NormalProxyProxy.objects.untranslated().use_fallbacks().get(), NormalProxyProxy))
