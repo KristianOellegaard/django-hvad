95163307d414cf62199f4da69f59f2f089a19d74
Jonas Obrist <ojiidotch@gmail.com>
2013-05-21 22:53:45 +0900

    Fixed Python 2.6 compatibility

--- hvad/compat/method_type.py
+++ hvad/compat/method_type.py
@@ -2,7 +2,7 @@
 import sys
 from types import MethodType as OriginalMethodType
 
-if sys.version_info.major == 2:
+if sys.version_info[0] == 2:
     def MethodType(function, instance):
         return OriginalMethodType(function, instance, instance.__class__)
 else:
