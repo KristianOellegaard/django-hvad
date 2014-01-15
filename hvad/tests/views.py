6b07bcbc56f29d13c23ca67d5acedef3cb15552f
Chris Wilson <chris+github@qwirx.com>
2014-01-15 12:14:44 +0000

    Use assertEqual in tests instead of assertTrue.
    Makes test failure messages more useful. No practical change except that
    one wrong assertion is fixed:
    
    	self.assertTrue(response.status_code, 200)

--- hvad/tests/views.py
+++ hvad/tests/views.py
@@ -17,35 +17,38 @@ class ViewsTest(NaniTestCase):
     def test_update_view_get(self):
         with LanguageOverride("en"):
             response = self.client.get(reverse('update_normal', args=[self.object.id]))
-            self.assertTrue(response.status_code== 200)
+            self.assertEqual(response.status_code, 200)
 
             response = self.client.get(reverse('update_normal_slug', kwargs={'slug': self.object.shared_field}))
-            self.assertTrue(response.status_code == 200)
+            self.assertEqual(response.status_code, 200)
 
             response = self.client.get(reverse('update_normal', args=[self.object.id]) + "?%s=da" % TranslatableUpdateView.query_language_key)
-            self.assertTrue(response.status_code == 200)
+            self.assertEqual(response.status_code, 200)
 
             response = self.client.get(reverse('update_normal', args=[self.object.id * 100]) + "?%s=da" % TranslatableUpdateView.query_language_key)
-            self.assertTrue(response.status_code == 404)
+            self.assertEqual(response.status_code, 404)
 
     def test_update_view_post(self):
         with LanguageOverride("en"):
             translated_string = u"some english translation"
-            response = self.client.post(reverse('update_normal', args=[self.object.id]),
-                                                data={
-                                                    'shared_field': 'some value',
-                                                    'translated_field': translated_string,
-                                                })
-            self.assertTrue(response.status_code == 302)
+            url = reverse('update_normal', args=[self.object.id])
+            response = self.client.post(url,
+                data={
+                    'shared_field': 'some value',
+                    'translated_field': translated_string,
+                })
+            self.assertEqual(response.status_code, 302)
             obj = Normal.objects.language().filter(pk=self.object.id).get()
-            self.assertTrue(obj.translated_field == translated_string)
-
+            self.assertEqual(obj.translated_field, translated_string)
 
             translated_string = u"svenne banan æøå"
-            response = self.client.post(reverse('update_normal', args=[self.object.id]) + "?%s=da" % TranslatableUpdateView.query_language_key,data={
-                                                    'shared_field': 'some value',
-                                                    'translated_field': translated_string,
-                                                })
-            self.assertTrue(response.status_code, 200)
+            response = self.client.post(
+                url + "?%s=da" % TranslatableUpdateView.query_language_key,
+                data={
+                    'shared_field': 'some value',
+                    'translated_field': translated_string,
+                })
+            self.assertEqual(response.status_code, 302)
             obj = Normal.objects.language("da").filter(pk=self.object.id).get()
-            self.assertTrue(obj.translated_field == translated_string)
+            self.assertEqual(obj.translated_field, translated_string)
+
