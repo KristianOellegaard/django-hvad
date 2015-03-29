74c4a0c9055d0c4ab75343ab6a0fb4fdfcfb7d90
Ivan Vasic <ivasic@gmail.com>
2015-03-29 22:04:47 +0200

    Fix setup crash caused by default character encoding on some systems

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from hvad import __version__ as version
 
-with open('README.rst') as f:
+with open('README.rst', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
