2a0c61e46be4cb1af71b9227ba4c865df668e121
Julien Hartmann <julien@etherdream.org>
2015-11-10 02:51:33 +0100

    Refactor translation model creation so it works with lazy initialization of Django1.9.

--- hvad/models.py
+++ hvad/models.py
@@ -29,160 +29,177 @@ def update_settings(*args, **kwargs):
 
 #===============================================================================
 
-def _split_together(constraints, fields, meta, name):
-    sconst, tconst = [], []
-    if name in meta:
-        # raise in 1.4, remove in 1.6
-        warnings.warn('Passing \'%s\' to TranslatedFields is deprecated. Please use '
-                      'Please Meta.%s instead.' % (name, name), DeprecationWarning)
-        tconst.extend(meta[name])
-
-    for constraint in constraints:
-        if all(item in fields for item in constraint):
-            tconst.append(constraint)
-        elif not any(item in fields for item in constraint):
-            sconst.append(constraint)
-        else:
-            raise ImproperlyConfigured(
-                'Constraints in Meta.%s cannot mix translated and '
-                'untranslated fields, such as %r.' % (name, constraint))
-    return sconst, tconst
-
-def create_translations_model(model, related_name, meta, **fields):
-    """
-    Create the translations model for the shared model 'model'.
-    'related_name' is the related name for the reverse FK from the translations
-    model.
-    'meta' is a (optional) dictionary of attributes for the translations model's
-    inner Meta class.
-    'fields' is a dictionary of fields to put on the translations model.
-    
-    Two fields are enforced on the translations model:
-    
-        language_code: A 15 char, db indexed field.
-        master: A ForeignKey back to the shared model.
-        
-    Those two fields are unique together, this get's enforced in the inner Meta
-    class of the translations table
-    """
-
-    # Build a list of translation models from base classes. Depth-first scan.
-    abstract = model._meta.abstract
-    translation_bases = []
-    translation_base_fields = set()
-    scan_bases = list(reversed(model.__bases__)) # backwards so we can use pop/extend
-    while scan_bases:
-        base = scan_bases.pop()
-        if not issubclass(base, TranslatableModel) or base is TranslatableModel:
-            continue
-        if not base._meta.abstract:
-            raise TypeError(
-                'Multi-table inheritance of translatable models is not supported. '
-                'Concrete model %s is not a valid base model for %s.' % (
-                    base._meta.model_name if django.VERSION >= (1, 6) else base._meta.module_name,
-                    model._meta.model_name if django.VERSION >= (1, 6) else model._meta.module_name))
-        try:
-            # The base may have translations model, then just inherit that
-            translation_bases.append(base._meta.translations_model)
-            translation_base_fields.update(field.name for field in base._meta.translations_model._meta.fields)
-        except AttributeError:
-            # But it may not, and simply inherit other abstract bases, scan them
-            scan_bases.extend(reversed(base.__bases__))
-    translation_bases.append(BaseTranslationModel)
-
-    # Create translation model Meta
-    meta = meta or {}
-    meta['abstract'] = abstract
-    meta['db_tablespace'] = model._meta.db_tablespace
-    meta['managed'] = model._meta.managed
-    if model._meta.order_with_respect_to in fields:
-        raise ValueError(
-            'Using a translated fields in %s.Meta.order_with_respect_to is ambiguous '
-            'and hvad does not support it.' %
-            model._meta.model_name if django.VERSION >= (1, 6) else model._meta.module_name)
-
-    sconst, tconst = _split_together(
-        model._meta.unique_together,
-        translation_base_fields.union(fields).union(('language_code',)),
-        meta,
-        'unique_together'
-    )
-    model._meta.unique_together = tuple(sconst)
-    if django.VERSION >= (1, 7):
-        model._meta.original_attrs['unique_together'] = tuple(sconst)
-    meta['unique_together'] = tuple(tconst)
-    if django.VERSION >= (1, 5):
-        sconst, tconst = _split_together(
-            model._meta.index_together,
-            translation_base_fields.union(fields).union(('language_code',)),
-            meta,
-            'index_together'
-        )
-        model._meta.index_together = tuple(sconst)
-        if django.VERSION >= (1, 7):
-            model._meta.original_attrs['index_together'] = tuple(sconst)
-        meta['index_together'] = tuple(tconst)
-
-    if not abstract:
-        unique = [('language_code', 'master')]
-        meta['unique_together'] = list(meta.get('unique_together', [])) + unique
-    Meta = type('Meta', (object,), meta)
-
-    if not hasattr(Meta, 'db_table'):
-        Meta.db_table = model._meta.db_table + '%stranslation' % TABLE_NAME_SEPARATOR
-    Meta.app_label = model._meta.app_label
-    name = '%sTranslation' % model.__name__
-
-    # Create translation model
-    attrs = {}
-    attrs.update(fields)
-    attrs['Meta'] = Meta
-    attrs['__module__'] = model.__module__
-
-    if not abstract:
-        # If this class is abstract, we must not contribute management fields
-        attrs['objects'] = TranslationsModelManager()
-        attrs['language_code'] = models.CharField(max_length=15, db_index=True)
-        # null=True is so we can prevent cascade deletion
-        attrs['master'] = models.ForeignKey(model, related_name=related_name,
-                                            editable=False, null=True)
-    # Create and return the new model
-    translations_model = ModelBase(name, tuple(translation_bases), attrs)
-    if not abstract:
-        # Abstract models do not have a DNE class
-        bases = (model.DoesNotExist, translations_model.DoesNotExist,)
-        DNE = type('DoesNotExist', bases, {})
-        translations_model.DoesNotExist = DNE
-    opts = translations_model._meta
-    opts.shared_model = model
-
-    # We need to set it here so it is available when we scan subclasses
-    model._meta.translations_model = translations_model
-
-    # Register it as a global in the shared model's module.
-    # This is needed so that Translation model instances, and objects which
-    # refer to them, can be properly pickled and unpickled. The Django session
-    # and caching frameworks, in particular, depend on this behaviour.
-    mod = sys.modules[model.__module__]
-    setattr(mod, name, translations_model)
-
-    return translations_model
-
-
 class TranslatedFields(object):
-    """
-    Wrapper class to define translated fields on a model.
-    """
+    """ Wrapper class to define translated fields on a model. """
+
     def __init__(self, meta=None, **fields):
+        self.meta = meta or {}
         self.fields = fields
-        self.meta = meta
 
-    def contribute_to_class(self, cls, name):
-        """
-        Called from django.db.models.base.ModelBase.__new__
-        """
-        create_translations_model(cls, name, self.meta, **self.fields)
+    @staticmethod
+    def _split_together(constraints, fields, meta, name):
+        sconst, tconst = [], []
+        if name in meta:
+            # raise in 1.4, remove in 1.6
+            warnings.warn('Passing \'%s\' to TranslatedFields is deprecated. Please use '
+                        'Please Meta.%s instead.' % (name, name), DeprecationWarning)
+            tconst.extend(meta[name])
 
+        for constraint in constraints:
+            if all(item in fields for item in constraint):
+                tconst.append(constraint)
+            elif not any(item in fields for item in constraint):
+                sconst.append(constraint)
+            else:
+                raise ImproperlyConfigured(
+                    'Constraints in Meta.%s cannot mix translated and '
+                    'untranslated fields, such as %r.' % (name, constraint))
+        return sconst, tconst
+
+    def contribute_to_class(self, model, name):
+        if model._meta.order_with_respect_to in self.fields:
+            raise ValueError(
+                'Using a translated fields in %s.Meta.order_with_respect_to is ambiguous '
+                'and hvad does not support it.' %
+                model._meta.model_name if django.VERSION >= (1, 6) else model._meta.module_name
+            )
+        if hasattr(model._meta, 'translations_model'):
+            raise ImproperlyConfigured(
+                "A TranslatableModel can only define one set of "
+                "TranslatedFields, %r defines more than one." % model
+            )
+        translations_model = self.create_translations_model(model, name)
+        model._meta.translations_model = translations_model
+        if not model._meta.abstract:
+            self.contribute_translations(model, translations_model, name)
+
+    def create_translations_model(self, model, related_name):
+        """ Create the translations model for a shared model.
+            model -- the model class to create translations for
+            related_name -- the related name for the reverse FK from the translations model.
+        """
+        model_name = '%sTranslation' % model.__name__
+        translation_bases, translation_base_fields = self._scan_model_bases(model)
+
+        attrs = self.fields.copy()
+        attrs.update({
+            'Meta': self._build_meta_class(
+                model, translation_base_fields.union(self.fields).union(('language_code',))
+            ),
+            '__module__': model.__module__,
+        })
+
+        if not model._meta.abstract:
+            attrs.update({
+                # If this class is abstract, we must not contribute management fields
+                'objects': TranslationsModelManager(),
+                'language_code': models.CharField(max_length=15, db_index=True),
+                # Nullable so we can prevent cascade deletion
+                'master': models.ForeignKey(model, related_name=related_name, editable=False, null=True),
+            })
+
+        # Create the new model
+        translations_model = ModelBase(model_name, tuple(translation_bases), attrs)
+        translations_model._meta.shared_model = model
+        if not model._meta.abstract:
+            # Abstract models do not have a DNE class
+            bases = (model.DoesNotExist, translations_model.DoesNotExist,)
+            translations_model.DoesNotExist = type('DoesNotExist', bases, {})
+
+        # Register it as a global in the shared model's module.
+        # This is needed so that Translation model instances, and objects which
+        # refer to them, can be properly pickled and unpickled. The Django session
+        # and caching frameworks, in particular, depend on this behaviour.
+        setattr(sys.modules[model.__module__], model_name, translations_model)
+        return translations_model
+
+    def _scan_model_bases(self, model):
+        """ Scan the model class' bases, collecting translated fields """
+        bases, fields = list(), set()
+        scan_bases = list(reversed(model.__bases__))
+        while scan_bases:
+            base = scan_bases.pop()
+            if base is TranslatableModel or not issubclass(base, TranslatableModel):
+                continue
+            if not base._meta.abstract:
+                raise TypeError(
+                    'Multi-table inheritance of translatable models is not supported. '
+                    'Concrete model %s is not a valid base model for %s.' % (
+                        base._meta.model_name if django.VERSION >= (1, 6) else base._meta.module_name,
+                        model._meta.model_name if django.VERSION >= (1, 6) else model._meta.module_name)
+                )
+            # The base may have translations model, then just inherit that
+            if hasattr(base._meta, 'translations_model'):
+                bases.append(base._meta.translations_model)
+                fields.update(field.name for field in base._meta.translations_model._meta.fields)
+            else:
+                # But it may not, and simply inherit other abstract bases, scan them
+                scan_bases.extend(reversed(base.__bases__))
+        bases.append(BaseTranslationModel)
+        return bases, fields
+
+    def _build_meta_class(self, model, tfields):
+        """ Create the Meta class for the translation model
+            model -- the shared model
+            tfields -- the list of names of all fields, direct and inherited
+        """
+        abstract = model._meta.abstract
+        meta = self.meta.copy()
+        meta.update({
+            'abstract': abstract,
+            'db_tablespace': model._meta.db_tablespace,
+            'managed': model._meta.managed,
+            'app_label': model._meta.app_label,
+            'db_table': meta.get('db_table',
+                                 '%s%stranslation' % (model._meta.db_table, TABLE_NAME_SEPARATOR)),
+        })
+        if django.VERSION >= (1, 7):
+            meta['default_permissions'] = ()
+
+        # Split fields in Meta.unique_together
+        sconst, tconst = self._split_together(
+            model._meta.unique_together, tfields, meta, 'unique_together'
+        )
+        model._meta.unique_together = tuple(sconst)
+        if django.VERSION >= (1, 7):
+            model._meta.original_attrs['unique_together'] = tuple(sconst)
+        meta['unique_together'] = tuple(tconst)
+        if not abstract:
+            meta['unique_together'] += (('language_code', 'master'),)
+
+        # Split fields in Meta.index_together
+        if django.VERSION >= (1, 5):
+            sconst, tconst = self._split_together(
+                model._meta.index_together, tfields, meta, 'index_together'
+            )
+            model._meta.index_together = tuple(sconst)
+            if django.VERSION >= (1, 7):
+                model._meta.original_attrs['index_together'] = tuple(sconst)
+            meta['index_together'] = tuple(tconst)
+
+        return type('Meta', (object,), meta)
+
+    def contribute_translations(self, model, translations_model, related_name):
+        """ Contribute translations model to the Meta class and set descriptors """
+
+        model._meta.translations_accessor = related_name
+        model._meta.translations_cache = '%s_cache' % related_name
+
+        # Set descriptors
+        ignore_fields = ('pk', 'master', 'master_id', translations_model._meta.pk.name)
+        for field in translations_model._meta.fields:
+            if field.name in ignore_fields:
+                continue
+            if field.name == 'language_code':
+                attr = LanguageCodeAttribute(model._meta)
+            else:
+                attr = TranslatedAttribute(model._meta, field.name)
+                attname = field.get_attname()
+                if attname and attname != field.name:
+                    setattr(model, attname, TranslatedAttribute(model._meta, attname))
+            setattr(model, field.name, attr)
+
+#===============================================================================
 
 class BaseTranslationModel(models.Model):
     """
@@ -407,37 +424,6 @@ class TranslatableModel(models.Model):
 
 #=============================================================================
 
-def contribute_translations(cls, rel):
-    """
-    Contribute translations options to the inner Meta class and set the
-    descriptors.
-
-    This get's called from prepare_translatable_model
-    """
-    opts = cls._meta
-    opts.translations_accessor = rel.get_accessor_name()
-    if django.VERSION >= (1, 8):
-        opts.translations_model = rel.field.model
-    else:
-        opts.translations_model = rel.model
-    opts.translations_cache = '%s_cache' % rel.get_accessor_name()
-    trans_opts = opts.translations_model._meta
-
-    # Set descriptors
-    ignore_fields = ('pk', 'master', 'master_id', opts.translations_model._meta.pk.name)
-    for field in trans_opts.fields:
-        if field.name in ignore_fields:
-            continue
-        if field.name == 'language_code':
-            attr = LanguageCodeAttribute(opts)
-        else:
-            attr = TranslatedAttribute(opts, field.name)
-            attname = field.get_attname()
-            if attname and attname != field.name:
-                setattr(cls, attname, TranslatedAttribute(opts, attname))
-        setattr(cls, field.name, attr)
-
-
 def prepare_translatable_model(sender, **kwargs):
     model = sender
     if not issubclass(model, TranslatableModel) or model._meta.abstract:
@@ -448,40 +434,18 @@ def prepare_translatable_model(sender, **kwargs):
             "TranslationManager instance or an instance of a subclass of "
             "TranslationManager, the default manager of %r is not." % model)
 
-    # If this is a proxy model, get the concrete one
-    concrete_model = model._meta.concrete_model if model._meta.proxy else model
+    if model._meta.proxy:
+        model._meta.translations_accessor = model._meta.concrete_model._meta.translations_accessor
+        model._meta.translations_model = model._meta.concrete_model._meta.translations_model
+        model._meta.translations_cache = model._meta.concrete_model._meta.translations_cache
 
-    # Find the instance of TranslatedFields in the concrete model's dict
-    # We cannot use _meta.get_fields here as app registry is not ready yet.
-    found = None
-    for relation in list(concrete_model.__dict__.keys()):
-        try:
-            obj = getattr(model, relation)
-            if django.VERSION >= (1, 8):
-                shared_model = obj.related.field.model._meta.shared_model
-            else:
-                shared_model = obj.related.model._meta.shared_model
-        except AttributeError:
-            continue
-        if shared_model is concrete_model:
-            if found:
-                raise ImproperlyConfigured(
-                    "A TranslatableModel can only define one set of "
-                    "TranslatedFields, %r defines more than one: %r on %r "
-                    "and %r on %r and possibly more" % (model, obj,
-                    obj.related.model, found, found.related.model))
-            # Mark as found but keep looking so we catch duplicates and raise
-            found = obj
-
-    if not found:
+    if not hasattr(model._meta, 'translations_model'):
         raise ImproperlyConfigured(
             "No TranslatedFields found on %r, subclasses of "
             "TranslatableModel must define TranslatedFields." % model
         )
 
     #### Now we have to work ####
-
-    contribute_translations(model, found.related)
 
     # Ensure _base_manager cannot be TranslationManager despite use_for_related_fields
     # 1- it is useless unless default_class is overriden
